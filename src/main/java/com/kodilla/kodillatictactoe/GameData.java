bce019037ae4b15b014c3ad191100e6691fe1131
biedrekGit <128526304+biedrekGit@users.noreply.github.com>
2025-03-09 18:01:08 +0100

    First working version

--- src/main/java/com/kodilla/kodillatictactoe/GameData.java
+++ src/main/java/com/kodilla/kodillatictactoe/GameData.java
@@ -3,26 +3,26 @@ package com.kodilla.kodillatictactoe;
 public class GameData {
 
 
-    public int[] symbols = new int[9];
+    public int[] board = new int[9];
 
     public void addCircle(int number) {
-        if (symbols[number - 1] == 0) {
-            symbols[number - 1] = 1;
+        if (board[number - 1] == 0) {
+            board[number - 1] = 1;
         } else {
             System.out.println("The field is taken. Try again.");
         }
     }
 
     public void addCross(int number) {
-        if (symbols[number - 1] == 0) {
-            symbols[number - 1] = 4;
+        if (board[number - 1] == 0) {
+            board[number - 1] = 4;
         } else {
             System.out.println("The field is taken. Try again.");
         }
     }
 
-    public int[] getSymbols() {
-        return symbols;
+    public int[] getBoard() {
+        return board;
     }
 
     public boolean gameEnds() {
@@ -30,8 +30,8 @@ public class GameData {
         //scenario 1: there are 3 identical symbols horizontally
         int counter = 0;
         int lineSum = 0;
-        for (int i = 0; i < symbols.length; i++) {
-            lineSum = lineSum + symbols[i];
+        for (int i = 0; i < board.length; i++) {
+            lineSum = lineSum + board[i];
             counter++;
             if (counter == 3) {
                 if (lineSum == 3 || lineSum == 12) {
@@ -46,21 +46,21 @@ public class GameData {
         //scenario 2: there are 3 identical symbols vertically
         int columnSum;
         for (int i = 0; i < 3; i++) {
-            columnSum = symbols[i] + symbols[i + 3] + symbols[i + 6];
+            columnSum = board[i] + board[i + 3] + board[i + 6];
             if (columnSum == 3 || columnSum == 12) {
                 result = true;
                 break;
             }
         }
         // scenario 3: there are 3 identical symbols diagonally
-        int diagonalSum1 = symbols[0] + symbols[4] + symbols[8];
-        int diagonalSum2 = symbols[2] + symbols[4] + symbols[6];
+        int diagonalSum1 = board[0] + board[4] + board[8];
+        int diagonalSum2 = board[2] + board[4] + board[6];
         if ((diagonalSum1 == 3 || diagonalSum1 == 12) || (diagonalSum2 == 3 || diagonalSum2 == 12)) {
             result = true;
         }
         // scenario 4: there are no free fields left
         int zeroCounter = 0;
-        for (int i : symbols) {
+        for (int i : board) {
             if (i == 0) {
                 zeroCounter++;
             }
--- src/main/java/com/kodilla/kodillatictactoe/Main.java
+++ src/main/java/com/kodilla/kodillatictactoe/Main.java
@@ -8,19 +8,19 @@ public class Main {
         GameData game1 = new GameData();
         System.out.println("Game  1 starts");
         while (true) {
-            GamePresentation.showGameBoard(game1.getSymbols());
+            GamePresentation.showGameBoard(game1.getBoard());
             if (game1.gameEnds()) {
                 break;
             }
             System.out.println("Circle turn. Please choose field:");
-            int circleInput = GamePresentation.chooseField(game1.getSymbols());
+            int circleInput = GamePresentation.chooseField(game1.getBoard());
             game1.addCircle(circleInput);
-            GamePresentation.showGameBoard(game1.getSymbols());
+            GamePresentation.showGameBoard(game1.getBoard());
             if (game1.gameEnds()) {
                 break;
             }
             System.out.println("Cross turn. Please choose field:");
-            int crossInput = GamePresentation.chooseField(game1.getSymbols());
+            int crossInput = GamePresentation.chooseField(game1.getBoard());
             game1.addCross(crossInput);
         }
         System.out.println("Game ended.");

3d96497735d6b1e7ef5a81f4b8d313ed39da9f36
biedrekGit <128526304+biedrekGit@users.noreply.github.com>
2025-03-09 13:05:52 +0100

    First commit

---
+++ src/test/java/com/kodilla/kodillatictactoe/KodillaTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillatictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaTicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
